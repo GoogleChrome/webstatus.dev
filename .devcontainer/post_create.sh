036b306698a1bb061433f01addb77ce0a62ddbcc
James Scott <jamescscott@google.com>
2023-08-29 20:53:57 +0000

    Install gcloud differently
    This will allow various components to be installed via gcloud.
    If installed via package manager, it does not work.
    
    Change-Id: I66c6b3e77554dd04407e551617dcada4f8653c46

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Install special version until https://github.com/GoogleContainerTools/skaffold/pull/9008 is released
-curl -Lo skaffold https://github.com/jcscottiii/skaffold/releases/download/9006-workaround/skaffold && sudo install skaffold /usr/local/bin/ && rm skaffold
-
 # For AMD64 / x86_64
 [ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
 # For ARM64
