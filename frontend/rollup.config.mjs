6c6d18dbf9fa1c8b41b5666e7e3390f41aa066b8
Daniel LaLiberte <dlaliberte@google.com>
2024-02-13 22:31:56 +0000

    Include sourcemaps in js files, and ts source in those sourcemaps.

--- frontend/rollup.config.mjs
+++ frontend/rollup.config.mjs
@@ -16,13 +16,15 @@
 
 import path from 'path'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
+// import sourcemaps from 'rollup-plugin-sourcemaps'
 import terser from '@rollup/plugin-terser'
 import copy from 'rollup-plugin-copy'
 import css from 'rollup-plugin-css-only'
+// import typescript from '@rollup/plugin-typescript'
 
 export default [
   {
-    input: 'build/static/js/index.js',
+    input: 'dist/static/public/build/static/js/index.js',
     plugins: [
       // Entry point for application build; can specify a glob to build multiple
       // HTML files for non-SPA app
@@ -32,6 +34,7 @@ export default [
       // Resolve bare module specifiers to relative paths
       nodeResolve(),
       // Minify JS
+      // sourcemaps(),
       terser({
         ecma: 2020,
         module: true,
@@ -40,6 +43,10 @@ export default [
       css({
         output: 'css/styles.css'
       }),
+      // typescript({
+      //   sourceMap: true
+      //   // inlineSources: true
+      // }),
       copy({
         targets: [
           // Copy all files in img recursively.
@@ -50,13 +57,21 @@ export default [
           { src: '.postinstall/static/img/*', dest: 'dist/static/public/img' },
           // Copy the html file
           { src: 'src/static/index.html', dest: 'dist/static' }
+          // Copy sourcemaps from build
+          // {
+          //   src: 'dist/static/public/build/**/*.map',
+          //   dest: 'dist/static/public/build'
+          // },
+          // // Copy source .ts files
+          // { src: 'src/static/**/*.ts', dest: 'dist/static/public/ts' }
         ],
         verbose: true
       })
     ],
     output: {
       dir: 'dist/static/public',
-      entryFileNames: 'js/[name].js'
+      entryFileNames: 'js/[name].js',
+      sourcemap: 'inline'
     },
     onwarn: (warning) => {
       if (warning.code === 'THIS_IS_UNDECLARED') {
