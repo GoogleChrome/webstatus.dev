47b3ef7297455513df5fb8c6c4f0fd69eb31bff2
Daniel LaLiberte <dlaliberte@google.com>
2024-02-26 11:04:06 -0500

    Fill out flex-box.ts.  Use vbox on sidebar. (#49)
    * Fill out flex-box.ts.  Use vbox on sidebar.
    
    * Cleanup and rearrange for readability
    
    * Make baseline icons fit inside chips. (#51)
    
    * Setup spanner database tables for local development (#50)
    
    * Setup spanner database tables for local development
    
    Previously, the dockerfile for spanner only started the emulator. But nothing else.
    
    But after starting spanner, it has more steps that need to happen. [Docs](https://cloud.google.com/spanner/docs/emulator)
    
    In particular:
    1. Creating the instance node in the emulator
    2. Creating the database on the node
    3. Creating the tables
    
    A new script called run.sh has been created that runs steps 1 and 2 after
    the emulator starts using the same gcloud command from the docs.
    
    Use the wrench [tool](https://github.com/cloudspannerecosystem/wrench)
    to accomplish step 3. Wrench is a part of the [Cloud Spanner ecosystem](https://www.cloudspannerecosystem.dev/).
    Without wrench, we would need to maintain the separate migrations ourselves before using gcloud.
    
    Dockerfile changes:
    - Adjust the dockerfile to copy in the schema file and the new run.sh script
    - Download wrench in the dockerfile
    
    Spanner Skaffold changes:
    - Adjust the context (aka root directory) so that we can find and copy in the spanner schema.
    
    Other changes:
    - Fix the backend pod config to use the correct config for spanner.
    - Add new shell script to shell lint
    
    Change-Id: I02522fd9318afeee1a480a18d966e89af6fe0aba
    
    * remove manual migration handler
    
    Change-Id: Ia4cc6e4bc4ec480a8fc613b725fbd6a63c47f84d
    
    * update maintenance file
    
    Change-Id: I02be8eb45d0fc9606688358c644d75a8623d357d
    
    ---------
    
    Co-authored-by: Jason Robbins <jrobbins@gmail.com>
    Co-authored-by: James C Scott III <jcscottiii@users.noreply.github.com>

--- frontend/src/static/js/css/flex-box.ts
+++ frontend/src/static/js/css/flex-box.ts
@@ -17,25 +17,151 @@
 import {css} from 'lit';
 
 export const FLEX_BOX = css`
-  .hbox {
-    display: flex;
-    flex-direction: row;
-  }
-  .hbox.space-between {
-    justify-content: space-between;
-  }
-  .hbox > * {
-    display: inline-block;
-  }
-  .hbox > .hgrow {
-    flex-grow: 1;
-  }
-  .hbox > .hshrink {
-    flex-shrink: 2;
-  }
-
+  .hbox,
   .vbox {
     display: flex;
+  }
+
+  .hbox {
+    flex-direction: row;
+  }
+  .vbox {
     flex-direction: column;
+  }
+
+  /* Flexbox alignment */
+
+  .hbox.halign-items-start,
+  .vbox.valign-items-start {
+    justify-content: flex-start;
+  }
+  .hbox.halign-items-center,
+  .vbox.valign-items-center {
+    justify-content: center;
+  }
+  .hbox.halign-items-end,
+  .vbox.valign-items-end {
+    justify-content: flex-end;
+  }
+
+  /* Alignment in cross-axis. */
+  .hbox.valign-items-start,
+  .vbox.halign-items-start {
+    align-items: flex-start;
+  }
+
+  .hbox.valign-items-center,
+  .vbox.halign-items-center {
+    align-items: center;
+  }
+
+  .hbox.valign-items-end,
+  .vbox.halign-items-end {
+    align-items: flex-end;
+  }
+
+  /* Self/item alignment. */
+
+  .hbox > .valign-start,
+  .vbox > .halign-start,
+  .hbox.valign-items-start > *,
+  .vbox.halign-items-start > * {
+    align-self: flex-start;
+  }
+
+  .hbox > .valign-center,
+  .vbox > .halign-center,
+  .hbox.valign-items-center > *,
+  .vbox.halign-items-center > * {
+    align-self: center;
+  }
+
+  .hbox > .valign-end,
+  .vbox > .halign-end,
+  .hbox.valign-items-end > *,
+  .vbox.halign-items-end > * {
+    align-self: flex-end;
+  }
+
+  /* Stretch "alignment" */
+
+  .hbox > .hgrow,  /* obsolete - use .halign-stretch */
+  .hbox > .halign-stretch,
+  .vbox > .valign-stretch,
+  .hbox.halign-items-stretch > *,
+  .vbox.valign-items-stretch > * {
+    flex-grow: 1;
+  }
+  .hbox > .halign-stretch-2,
+  .vbox > .valign-stretch-2 {
+    flex-grow: 2;
+  }
+  .hbox > .halign-stretch-3,
+  .vbox > .valign-stretch-3 {
+    flex-grow: 3;
+  }
+
+  .hbox.valign-items-stretch,
+  .vbox.halign-items-stretch {
+    align-items: stretch;
+  }
+
+  .hbox > .valign-stretch,
+  .vbox > .halign-stretch,
+  .hbox.valign-items-stretch > *,
+  .vbox.halign-items-stretch > * {
+    align-self: stretch;
+  }
+
+  /* Space distribution */
+
+  .hbox.space-between {
+    /* obsolete - use .halign-space-between */
+    justify-content: space-between;
+  }
+
+  .hbox.halign-items-space-around,
+  .vbox.valign-items-space-around {
+    justify-content: space-around;
+  }
+  .hbox.valign-items-space-around,
+  .vbox.halign-items-space-around {
+    align-content: space-around;
+  }
+
+  .hbox.halign-items-space-between,
+  .vbox.valign-items-space-between {
+    justify-content: space-between;
+  }
+  .hbox.valign-items-space-between,
+  .vbox.halign-items-space-between {
+    align-content: space-between;
+  }
+
+  .hbox.halign-items-space-evenly,
+  .vbox.valign-items-space-evenly {
+    justify-content: space-evenly;
+  }
+  .hbox.valign-items-space-evenly,
+  .vbox.halign-items-space-evenly {
+    align-content: space-evenly;
+  }
+
+  .hbox > .spacer,
+  .hbox > .spacer-1,
+  .vbox > .spacer,
+  .vbox > .spacer-1 {
+    flex-grow: 1;
+    visibility: hidden;
+  }
+  .hbox > .spacer-2,
+  .vbox > .spacer-2 {
+    flex-grow: 2;
+    visibility: hidden;
+  }
+  .hbox > .spacer-3,
+  .vbox > .spacer-3 {
+    flex-grow: 3;
+    visibility: hidden;
   }
 `;
