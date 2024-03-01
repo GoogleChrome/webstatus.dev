a523b4356c4e6476c6f4c556de6f6ea0b55ec37f
James Scott <jamescscott@google.com>
2024-03-01 15:51:10 +0000

    Add code to insert Browser Releases
    This is splitting https://github.com/GoogleChrome/webstatus.dev/pull/54
    
    This introduces the code to insert browser releases.
    
    The schema for the table is introduced in https://github.com/GoogleChrome/webstatus.dev/pull/59
    
    This data will come from https://github.com/mdn/browser-compat-data/tree/main/browsers
    Specifically, the github release's data.json.
    
    It will do nothing on update. In the future, we could allow for corrections of specific fields.
    
    Change-Id: Ic876dbf6be8924e540d90c0bd9ef5853f93885a1

---
+++ lib/gcpspanner/browser_releases_test.go
@@ -0,0 +1,108 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gcpspanner
+
+import (
+	"context"
+	"errors"
+	"slices"
+	"testing"
+	"time"
+
+	"cloud.google.com/go/spanner"
+	"google.golang.org/api/iterator"
+)
+
+func getSampleBrowserReleases() []BrowserRelease {
+	// nolint: dupl // Okay to duplicate for tests
+	return []BrowserRelease{
+		{
+			BrowserName:    "fooBrowser",
+			BrowserVersion: "0.0.0",
+			ReleaseDate:    time.Date(2000, time.January, 1, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			BrowserName:    "barBrowser",
+			BrowserVersion: "0.0.0",
+			ReleaseDate:    time.Date(2000, time.January, 2, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			BrowserName:    "fooBrowser",
+			BrowserVersion: "1.0.0",
+			ReleaseDate:    time.Date(2000, time.February, 1, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			BrowserName:    "barBrowser",
+			BrowserVersion: "1.0.0",
+			ReleaseDate:    time.Date(2000, time.February, 2, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			BrowserName:    "fooBrowser",
+			BrowserVersion: "2.0.0",
+			ReleaseDate:    time.Date(2000, time.March, 1, 0, 0, 0, 0, time.UTC),
+		},
+		{
+			BrowserName:    "barBrowser",
+			BrowserVersion: "2.0.0",
+			ReleaseDate:    time.Date(2000, time.March, 2, 0, 0, 0, 0, time.UTC),
+		},
+	}
+}
+
+// Helper method to get all the browser releases in a stable order.
+// nolint: lll
+func (c *Client) ReadAllBrowserReleases(ctx context.Context, _ *testing.T) ([]BrowserRelease, error) {
+	stmt := spanner.NewStatement("SELECT BrowserName, BrowserVersion, ReleaseDate FROM BrowserReleases ORDER BY ReleaseDate ASC")
+	iter := c.Single().Query(ctx, stmt)
+	defer iter.Stop()
+
+	var ret []BrowserRelease
+	for {
+		row, err := iter.Next()
+		if errors.Is(err, iterator.Done) {
+			break // End of results
+		}
+		if err != nil {
+			return nil, errors.Join(ErrInternalQueryFailure, err)
+		}
+		var release SpannerBrowserRelease
+		if err := row.ToStruct(&release); err != nil {
+			return nil, errors.Join(ErrInternalQueryFailure, err)
+		}
+		ret = append(ret, release.BrowserRelease)
+	}
+
+	return ret, nil
+}
+
+func TestInsertBrowserRelease(t *testing.T) {
+	client := getTestDatabase(t)
+	ctx := context.Background()
+	sampleBrowserReleases := getSampleBrowserReleases()
+	for _, release := range sampleBrowserReleases {
+		err := client.InsertBrowserRelease(ctx, release)
+		if err != nil {
+			t.Errorf("unexpected error during insert. %s", err.Error())
+		}
+	}
+
+	releases, err := client.ReadAllBrowserReleases(ctx, t)
+	if err != nil {
+		t.Errorf("unexpected error during read all. %s", err.Error())
+	}
+	if !slices.Equal[[]BrowserRelease](sampleBrowserReleases, releases) {
+		t.Errorf("unequal releases. expected %+v actual %+v", sampleBrowserReleases, releases)
+	}
+}
