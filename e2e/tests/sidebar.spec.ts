af1fb5c041e531c11dbf72f56b649ff457871f94
Daniel LaLiberte <dlaliberte@google.com>
2024-03-03 18:26:22 +0000

    Fix playwright tests and update snapshots

--- e2e/tests/sidebar.spec.ts
+++ e2e/tests/sidebar.spec.ts
@@ -19,6 +19,10 @@ import {test, expect} from '@playwright/test';
 test('matches the screenshot', async ({page}) => {
   await page.goto('http://localhost:5555/');
 
+  // Show the sidebar menu
+  const menuButton = page.locator('#menu-button');
+  await menuButton.click();
+
   const sidebar = page.locator('webstatus-sidebar');
   await expect(sidebar).toHaveScreenshot('sidebar.png');
 });
