772b1aec24c1511a63af04cbc86b408572fbc3e4
James C Scott III <jcscottiii@users.noreply.github.com>
2024-03-02 19:08:00 -0500

    Add code to insert browser availabilities (#63)
    This is splitting https://github.com/GoogleChrome/webstatus.dev/pull/54
    
    This introduces the code to insert browser availability for a feature.
    
    The schema for the table is introduced in https://github.com/GoogleChrome/webstatus.dev/pull/59
    
    This data will come from the web features repo. Using the versions there, it will match
    up with the versions in the browser releases table to give users when the feature was release.
    
    It will do nothing on update. In the future, we could allow for corrections of specific fields.
    
    Change-Id: I26dcc6bb4fd7f4c83e82d0bcb7cb3b69f2c71786

---
+++ lib/gcpspanner/browser_availabilities.go
@@ -0,0 +1,80 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gcpspanner
+
+import (
+	"context"
+	"errors"
+
+	"cloud.google.com/go/spanner"
+	"google.golang.org/grpc/codes"
+)
+
+const browserFeatureAvailabilitiesTable = "BrowserFeatureAvailabilities"
+
+// SpannerBrowserFeatureAvailability is a wrapper for the browser availability
+// information for a feature stored in spanner. For now, it is the same. But we
+// keep this structure to be consistent to the other database models.
+type SpannerBrowserFeatureAvailability struct {
+	BrowserFeatureAvailability
+}
+
+// BrowserFeatureAvailability contains availability information for a particular
+// feature in a browser.
+type BrowserFeatureAvailability struct {
+	BrowserName    string
+	BrowserVersion string
+	FeatureID      string
+}
+
+// InsertBrowserFeatureAvailability will insert the given browser feature availability.
+// If the feature availability, does not exist, it will insert a new feature availability.
+// If the feature availability exists, it currently does nothing and keeps the existing as-is.
+// nolint: dupl // TODO. Will refactor for common patterns.
+func (c *Client) InsertBrowserFeatureAvailability(
+	ctx context.Context,
+	featureAvailability BrowserFeatureAvailability) error {
+	_, err := c.ReadWriteTransaction(ctx, func(ctx context.Context, txn *spanner.ReadWriteTransaction) error {
+		_, err := txn.ReadRow(
+			ctx,
+			browserFeatureAvailabilitiesTable,
+			spanner.Key{featureAvailability.FeatureID, featureAvailability.BrowserName},
+			[]string{
+				"BrowserVersion",
+			})
+		if err != nil {
+			// Received an error other than not found. Return now.
+			if spanner.ErrCode(err) != codes.NotFound {
+				return errors.Join(ErrInternalQueryFailure, err)
+			}
+			m, err := spanner.InsertOrUpdateStruct(browserFeatureAvailabilitiesTable, featureAvailability)
+			if err != nil {
+				return errors.Join(ErrInternalQueryFailure, err)
+			}
+			err = txn.BufferWrite([]*spanner.Mutation{m})
+			if err != nil {
+				return errors.Join(ErrInternalQueryFailure, err)
+			}
+		}
+		// For now, do not overwrite anything for releases.
+		return nil
+
+	})
+	if err != nil {
+		return errors.Join(ErrInternalQueryFailure, err)
+	}
+
+	return nil
+}
