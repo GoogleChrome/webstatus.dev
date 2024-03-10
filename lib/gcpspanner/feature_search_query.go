6934268414bd5ffb898a64bb5eaa1e2a09d4c965
James Scott <jamescscott@google.com>
2024-03-10 19:19:01 +0000

    Add query builder for feature search with Spanner support
    Introduce FeatureSearchFilterBuilder to dynamically generate Spanner queries.
    Includes parameter handling, filter creation, and correct parentheses for complex queries.
    
    This uses the built SearchNode tree to create filters on the feature search query.
    
    The SearchNode tree is built from the implemented ANTLR visitor from a previous PR.
    
    This code will replace the current filtering system (which will be done in a future PR).
    
    Change-Id: If501c04c6a687f1e01573ab876a98da9a1b4f983

--- lib/gcpspanner/feature_search_query.go
+++ lib/gcpspanner/feature_search_query.go
@@ -15,8 +15,137 @@
 package gcpspanner
 
 import (
+	"fmt"
+	"strings"
+
 	"cloud.google.com/go/spanner"
+	"github.com/GoogleChrome/webstatus.dev/lib/gcpspanner/searchtypes"
 )
+
+type FeatureSearchFilterBuilder struct {
+	paramCounter int
+	params       map[string]interface{}
+}
+
+func NewFeatureSearchFilterBuilder() *FeatureSearchFilterBuilder {
+	return &FeatureSearchFilterBuilder{
+		paramCounter: 0,
+		params:       nil,
+	}
+}
+
+type FeatureSearchCompiledFilter struct {
+	params map[string]interface{}
+	clause string
+}
+
+func (f FeatureSearchCompiledFilter) Params() map[string]interface{} {
+	return f.params
+}
+
+func (f FeatureSearchCompiledFilter) Clause() string {
+	return f.clause
+}
+
+// addParamGetName adds a parameter to the map that will be used in the spanner params map.
+// Afterwards, get the name of the parameter. Then increment the counter.
+func (b *FeatureSearchFilterBuilder) addParamGetName(param interface{}) string {
+	name := fmt.Sprintf("param%d", b.paramCounter)
+	b.params[name] = param
+
+	// Increment for the next parameter.
+	b.paramCounter++
+
+	return name
+}
+
+func (b *FeatureSearchFilterBuilder) Build(node *searchtypes.SearchNode) *FeatureSearchCompiledFilter {
+	// Ensure it is not nil
+	if node == nil ||
+		// Check for our root node.
+		node.Operator != searchtypes.OperatorRoot ||
+		// Currently root should only have at most one child.
+		// lib/gcpspanner/searchtypes/features_search_visitor.go
+		len(node.Children) != 1 {
+		return nil
+	}
+
+	//  Initialize the map and (re)set counter to 0
+	b.params = make(map[string]interface{})
+	b.paramCounter = 0
+
+	generatedFilters := b.traverseAndGenerateFilters(node.Children[0])
+	filterClause := strings.Join(generatedFilters, " AND ")
+
+	return &FeatureSearchCompiledFilter{
+		params: b.params,
+		clause: filterClause,
+	}
+}
+
+func (b *FeatureSearchFilterBuilder) traverseAndGenerateFilters(node *searchtypes.SearchNode) []string {
+	var filters []string
+
+	switch {
+	case node.IsOperator(): // Handle AND/OR operators
+		var childFilters []string // Collect child filters first
+		for _, child := range node.Children {
+			childFilters = append(childFilters, b.traverseAndGenerateFilters(child)...)
+		}
+
+		// Join child filters using the current node's operator
+		if len(childFilters) > 0 {
+			joiner := " AND "
+			if node.Operator == searchtypes.OperatorOR {
+				joiner = " OR "
+			}
+			filterString := strings.Join(childFilters, joiner)
+
+			if strings.TrimSpace(filterString) != "" {
+				filters = append(filters, "("+filterString+")")
+			}
+
+		}
+
+	case node.Term != nil && (node.Operator == searchtypes.OperatorNone || node.Operator == searchtypes.OperatorNegation):
+		var filter string
+		switch node.Term.Identifier {
+		case searchtypes.IdentifierAvailableOn:
+			filter = b.availabilityFilter(node.Term.Value)
+		case searchtypes.IdentifierName:
+			filter = b.featureNameFilter(node.Term.Value)
+		case searchtypes.IdentifierBaselineStatus:
+			filter = b.baselineStatusFilter(node.Term.Value)
+		}
+		if filter != "" {
+			if node.Operator == searchtypes.OperatorNegation {
+				filter = "NOT (" + filter + ")"
+			}
+			filters = append(filters, "("+filter+")")
+		}
+	}
+
+	return filters
+}
+
+func (b *FeatureSearchFilterBuilder) availabilityFilter(browser string) string {
+	paramName := b.addParamGetName(browser)
+
+	return fmt.Sprintf(`wf.FeatureID IN (SELECT FeatureID FROM BrowserFeatureAvailabilities
+WHERE BrowserName = @%s)`, paramName)
+}
+
+func (b *FeatureSearchFilterBuilder) featureNameFilter(featureName string) string {
+	paramName := b.addParamGetName(featureName)
+
+	return fmt.Sprintf(`(wf.Name LIKE @%s OR wf.FeatureID LIKE @%s)`, paramName, paramName)
+}
+
+func (b *FeatureSearchFilterBuilder) baselineStatusFilter(baselineStatus string) string {
+	paramName := b.addParamGetName(baselineStatus)
+
+	return fmt.Sprintf(`fbs.Status = @%s`, paramName)
+}
 
 // Filterable modifies a query with a given filter.
 type Filterable interface {
