44698589049920e24c50bc53596a77fc2106feff
James C Scott III <jcscottiii@users.noreply.github.com>
2024-04-08 15:39:59 -0400

    Add the beginnings of the BCD Release workflow (#144)
    This change adds the high level BCD Release Workflow worker struct
    
    In that struct, it has a Process method which includes the high level steps that will be implemented in future changes.
    
    The BCDReleasesWorker implements a Work method that can distribute the work using the lib/workerpool interface.
    - This follows the same pattern as the WPT worker.
    
    Change-Id: Id617e54a65f703fddfdd399bcc01e908e9421816

---
+++ workflows/steps/services/bcd_consumer/pkg/workflow/bcd_releases_worker.go
@@ -0,0 +1,154 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package workflow
+
+import (
+	"context"
+	"io"
+	"log/slog"
+	"net/http"
+	"sync"
+
+	"github.com/GoogleChrome/webstatus.dev/lib/gcpspanner/spanneradapters/bcdconsumertypes"
+	"github.com/GoogleChrome/webstatus.dev/workflows/steps/services/bcd_consumer/pkg/data"
+)
+
+func NewBCDReleasesWorker(
+	dataGetter DataGetter,
+	dataParser DataParser,
+	dataFilter DataFilter,
+	dataStorer DataStorer,
+	repoOwner string,
+	repoName string,
+	releaseAssetFilename string,
+) *BCDReleasesWorker {
+	return &BCDReleasesWorker{
+		jobProcessor: BCDJobProcessor{
+			dataGetter:           dataGetter,
+			dataParser:           dataParser,
+			dataFilter:           dataFilter,
+			dataStorer:           dataStorer,
+			repoOwner:            repoOwner,
+			repoName:             repoName,
+			releaseAssetFilename: releaseAssetFilename,
+		},
+	}
+}
+
+type BCDReleasesWorker struct {
+	// Handles the processing of individual jobs
+	jobProcessor JobProcessor
+}
+
+func NewJobArguments(browsers []string) JobArguments {
+	return JobArguments{
+		browsers: browsers,
+	}
+}
+
+type JobArguments struct {
+	browsers []string // List of browsers that we will search for and store their respective release information.
+}
+
+// JobProcessor defines the contract for processing a single job within the BCD Releases workflow.
+type JobProcessor interface {
+	Process(
+		ctx context.Context,
+		job JobArguments) error
+}
+
+func (w BCDReleasesWorker) Work(
+	ctx context.Context, id int, wg *sync.WaitGroup, jobs <-chan JobArguments, errChan chan<- error) {
+	slog.Info("starting worker", "worker id", id)
+	defer wg.Done()
+
+	// Processes jobs received on the 'jobs' channel
+	for job := range jobs {
+		err := w.jobProcessor.Process(ctx, job)
+		if err != nil {
+			errChan <- err
+		}
+	}
+	// Do not close the shared error channel here.
+	// It will prevent others from returning their errors.
+}
+
+type DataGetter interface {
+	DownloadFileFromRelease(
+		ctx context.Context,
+		owner, repo string,
+		httpClient *http.Client,
+		filePattern string) (io.ReadCloser, error)
+}
+
+// DataParser describes the behavior to read raw bytes into the expected BCDData struct.
+type DataParser interface {
+	Parse(in io.ReadCloser) (*data.BCDData, error)
+}
+
+// DataFilter describes the behavior to take full BCDData and only filter for the applicable browser releases.
+type DataFilter interface {
+	FilterData(*data.BCDData, []string) ([]bcdconsumertypes.BrowserRelease, error)
+}
+
+// DataStorer describes the behavior to store the release information.
+type DataStorer interface {
+	InsertBrowserReleases(ctx context.Context, releases []bcdconsumertypes.BrowserRelease) error
+}
+
+type BCDJobProcessor struct {
+	dataGetter           DataGetter // Dependency for fetching data from the BCD repo.
+	dataParser           DataParser // Dependency for parsing BCD Data.
+	dataFilter           DataFilter // Dependency for filtering BCD Data into the anticipated data structure
+	dataStorer           DataStorer // Dependency for storing the BCD Data.
+	repoOwner            string
+	repoName             string
+	releaseAssetFilename string
+}
+
+func (p BCDJobProcessor) Process(
+	ctx context.Context,
+	job JobArguments) error {
+	// Step 1. Download the file.
+	file, err := p.dataGetter.DownloadFileFromRelease(
+		ctx,
+		p.repoOwner,
+		p.repoName,
+		http.DefaultClient,
+		p.releaseAssetFilename)
+	if err != nil {
+		return err
+	}
+
+	// Step 2. Parse the file.
+	data, err := p.dataParser.Parse(file)
+	if err != nil {
+		return err
+	}
+
+	// Step 3. Filter the data.
+	filteredData, err := p.dataFilter.FilterData(data, job.browsers)
+	if err != nil {
+		return err
+	}
+
+	// Step 4. Insert the browser release data.
+	err = p.dataStorer.InsertBrowserReleases(ctx, filteredData)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
