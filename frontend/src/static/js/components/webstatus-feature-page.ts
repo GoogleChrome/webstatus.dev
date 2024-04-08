89509686ddf92451b102ffc57d5f4a35186be595
Daniel LaLiberte <dlaliberte@google.com>
2024-04-08 18:46:45 +0000

    Cleanup

--- frontend/src/static/js/components/webstatus-feature-page.ts
+++ frontend/src/static/js/components/webstatus-feature-page.ts
@@ -36,16 +36,6 @@ import {
   renderWPTScore,
 } from './webstatus-overview-cells.js';
 
-import {
-  makeRandomDataForAllBrowserChannelCombos,
-  BrowsersParameter,
-  ALL_BROWSERS,
-  browserChannelDataMap,
-  browserChannelDataMapKey,
-  ChannelsParameter,
-} from './random-data.js';
-import {SlMenu, SlMenuItem} from '@shoelace-style/shoelace';
-
 @customElement('webstatus-feature-page')
 export class FeaturePage extends LitElement {
   _loadingTask: Task;
@@ -59,32 +49,6 @@ export class FeaturePage extends LitElement {
 
   @state()
   featureId!: string;
-
-  @state()
-  implementationProgressBrowsers: BrowsersParameter[] = ALL_BROWSERS;
-
-  // @state()
-  // implementationProgressChannels: ChannelsParameter[] = ALL_FEATURES;
-
-  @state()
-  implementationProgressChartOptions: google.visualization.LineChartOptions = {
-    title: 'Implementation Progress',
-    hAxis: {title: 'Date'},
-    vAxis: {title: 'Percent'},
-    series: {
-      0: {color: '#3367d6'},
-      1: {color: '#d63367'},
-    },
-  };
-
-  @state()
-  startDate: Date = new Date(2021, 1, 1);
-
-  @state()
-  endDate: Date = new Date(2024, 4, 1);
-
-  @state()
-  implementationProgress: Array<components['schemas']['WPTRunMetric']> = [];
 
   location!: {params: {featureId: string}; search: string}; // Set by router.
 
@@ -158,21 +122,6 @@ export class FeaturePage extends LitElement {
         .under-construction {
           min-height: 12em;
         }
-
-        /* Make the dropdown menu button icon rotate when the menu is open,
-          so it looks like sl-select. */
-        sl-dropdown > sl-button > sl-icon {
-          rotate: 0deg;
-          transition: var(--sl-transition-medium) rotate ease;
-        }
-        sl-dropdown[open] > sl-button > sl-icon {
-          rotate: -180deg;
-          transition: var(--sl-transition-medium) rotate ease;
-        }
-
-        #implementation-progress-chart {
-          min-height: 20em;
-        }
       `,
     ];
   }
@@ -190,155 +139,10 @@ export class FeaturePage extends LitElement {
     });
   }
 
-  setupImplementationProgressBrowsersHandler() {
-    // Get the implementation progress data browser selector.
-    const browserSelectorMenu = this.shadowRoot!.querySelector(
-      '#implementation-progress-browser-selector sl-menu'
-    ) as SlMenu;
-    // Add a listener to the browserSelectorMenu to update the list of
-    // browsers in implementationProgressBrowsers.
-    browserSelectorMenu.addEventListener('sl-select', event => {
-      const menu = event.target as SlMenu;
-      const menuItemsArray: Array<SlMenuItem> = Array.from(
-        menu.children
-      ).filter(child => child instanceof SlMenuItem) as Array<SlMenuItem>;
-
-      // Build the list of values of checked menu-items.
-      this.implementationProgressBrowsers = menuItemsArray
-        .filter(menuItem => menuItem.checked)
-        .map(menuItem => menuItem.value) as BrowsersParameter[];
-      // Regenerate data and redraw. We should instead just filter it.
-      this.setupImplementationProgressChart();
-    });
-  }
-
-  setupDateRangeHandler() {
-    const startDateInput = this.shadowRoot!.querySelector(
-      '#start-date'
-    ) as HTMLInputElement;
-    startDateInput.addEventListener('sl-blur', event => {
-      const currentStartDate = this.startDate;
-      this.startDate = new Date((event.target as HTMLInputElement).value);
-      if (this.startDate.getTime() === currentStartDate.getTime()) return;
-      // Regenerate data and redraw. We should instead just filter it.
-      this.setupImplementationProgressChart();
-    });
-    const endDateInput = this.shadowRoot!.querySelector(
-      '#end-date'
-    ) as HTMLInputElement;
-    endDateInput.addEventListener('sl-blur', event => {
-      const currentEndDate = this.endDate;
-      this.endDate = new Date((event.target as HTMLInputElement).value);
-      if (this.endDate.getTime() === currentEndDate.getTime()) return;
-      // Regenerate data and redraw. We should instead just filter it.
-      this.setupImplementationProgressChart();
-    });
-  }
-
-  setupImplementationProgressChart() {
-    makeRandomDataForAllBrowserChannelCombos(this.startDate, this.endDate);
-
-    google.charts.load('current', {
-      packages: ['corechart'],
-    });
-    google.charts.setOnLoadCallback(() => {
-      // Let's render a chart...
-      this.createImplementationProgressChart();
-    });
-
-    // Add window resize event handler to redraw the chart.
-    window.addEventListener('resize', () => {
-      this.createImplementationProgressChart();
-    });
-  }
-
   async firstUpdated(): Promise<void> {
     // TODO(jrobbins): Use routerContext instead of this.location so that
     // nested components could also access the router.
     this.featureId = this.location.params.featureId;
-  }
-
-  // Make a DataTable from the data in browserChannelDataMap
-  createImplementationProgressDataTableFromMap(
-    browsers: BrowsersParameter[],
-    channel: ChannelsParameter
-  ): google.visualization.DataTable {
-    const dataTable = new google.visualization.DataTable();
-    dataTable.addColumn('date', 'Date');
-    for (const browser of browsers) {
-      dataTable.addColumn('number', browser);
-    }
-    dataTable.addColumn('number', 'Total');
-
-    // Map from date to array of counts for each browser
-    const dateToBrowserDataMap = new Map<number, Array<number>>();
-    // Map from date to array of total_tests_count, the same for all browsers.
-    const dateToTotalTestsCountMap = new Map<number, number>();
-
-    // Merge data across all browsers into one array of rows.
-    for (const browser of browsers) {
-      const data = browserChannelDataMap.get(
-        browserChannelDataMapKey(browser, channel)
-      );
-      if (!data) continue;
-      for (const row of data) {
-        if (!row) continue;
-        const dateSeconds = new Date(row.run_timestamp).getTime();
-        const testPassCount = row.test_pass_count!;
-        if (!dateToBrowserDataMap.has(dateSeconds)) {
-          dateToBrowserDataMap.set(dateSeconds, [testPassCount]);
-          dateToTotalTestsCountMap.set(dateSeconds, row.total_tests_count!);
-        } else {
-          dateToBrowserDataMap.get(dateSeconds)!.push(testPassCount);
-        }
-      }
-    }
-
-    // Sort the dateToBrowserDataMap by dateSeconds
-    const data = Array.from(dateToBrowserDataMap.entries()).sort(
-      ([d1], [d2]) => d1 - d2
-    );
-
-    // For each date, add a row to the dataTable
-    for (const row of data) {
-      const dateSeconds = row[0];
-      const date = new Date(dateSeconds);
-      const browserCounts = row[1];
-      const total = dateToTotalTestsCountMap.get(dateSeconds)!;
-      dataTable.addRow([date, ...browserCounts, total]);
-    }
-    return dataTable;
-  }
-
-  createImplementationProgressChart(): void {
-    const data = this.createImplementationProgressDataTableFromMap(
-      this.implementationProgressBrowsers,
-      'stable'
-    );
-
-    // Add 2 weeks to this.endDate.
-    const endDate = new Date(this.endDate.getTime() + 1000 * 60 * 60 * 24 * 14);
-    const options = {
-      hAxis: {
-        title: '',
-        titleTextStyle: {color: '#333'},
-        viewWindow: {min: this.startDate, max: endDate},
-      },
-      vAxis: {minValue: 0},
-      legend: {position: 'top'},
-      chartArea: {left: 60, right: 16, top: 40, bottom: 40},
-    } as google.visualization.LineChartOptions;
-
-    const chart = new google.visualization.LineChart(
-      this.shadowRoot!.getElementById('implementation-progress-chart')!
-    );
-    chart.draw(data, options);
-  }
-
-  afterRenderWhenComplete() {
-    this.setupImplementationProgressBrowsersHandler();
-    this.setupDateRangeHandler();
-    this.setupImplementationProgressChart();
   }
 
   render(): TemplateResult | undefined {
@@ -434,47 +238,8 @@ export class FeaturePage extends LitElement {
   renderImplentationProgress(): TemplateResult {
     return html`
       <sl-card id="implementation-progress">
-        <div slot="header" class="hbox">
-          Implementation progress
-          <div class="spacer"></div>
-          <sl-select>
-            <sl-option> All features </sl-option>
-            <sl-option> how to select ? </sl-option>
-          </sl-select>
-          <sl-dropdown
-            id="implementation-progress-browser-selector"
-            multiple
-            stay-open-on-select
-            .value="${this.implementationProgressBrowsers.join(' ')}"
-          >
-            <sl-button slot="trigger">
-              <sl-icon slot="suffix" name="chevron-down"> </sl-icon>
-              Browsers
-            </sl-button>
-            <sl-menu>
-              <sl-menu-item type="checkbox" value="Chrome">
-                Chrome
-              </sl-menu-item>
-              <sl-menu-item type="checkbox" value="Edge"> Edge </sl-menu-item>
-              <sl-menu-item type="checkbox" value="Firefox">
-                Firefox
-              </sl-menu-item>
-              <sl-menu-item type="checkbox" value="Safari">
-                Safari
-              </sl-menu-item>
-            </sl-menu>
-          </sl-dropdown>
-        </div>
-        <webstatus-chart
-          id="implementation-progress-chart"
-          .data="${this.createImplementationProgressDataTableFromMap(
-            this.implementationProgressBrowsers,
-            'stable'
-          )}"
-          .options="${this.implementationProgressChartOptions}"
-        >
-          Loading chart...
-        </webstatus-chart>
+        <div slot="header">Implementation progress</div>
+        <p class="under-construction">Chart goes here...</p>
       </sl-card>
     `;
   }
