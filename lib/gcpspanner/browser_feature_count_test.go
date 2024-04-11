6eb914e4e3147965a35a50550abca63a0b082257
James C Scott III <jcscottiii@users.noreply.github.com>
2024-04-11 12:46:48 -0400

    Cumulative count of features over time for browser feature count (#151)
    * Cumulative count of features over time for browser feature count
    
    This change adds logic to the spanner layer to get the cumulative count
    of features for a browser over time.
    
    This is useful for the stats page.
    
    Change-Id: I0c2514a883e16605013b064010b09e548d313229
    
    * address feedback
    
    Change-Id: I01f4932eddc6b348fb451855570a28ee963f8e0a

---
+++ lib/gcpspanner/browser_feature_count_test.go
@@ -0,0 +1,143 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gcpspanner
+
+import (
+	"context"
+	"reflect"
+	"testing"
+	"time"
+)
+
+func loadDataForListBrowserFeatureCountMetric(ctx context.Context, t *testing.T, client *Client) {
+	webFeatures := []WebFeature{
+		{FeatureID: "FeatureX", Name: "Cool API"},
+		{FeatureID: "FeatureY", Name: "Super API"},
+		{FeatureID: "FeatureZ", Name: "Neat API"},
+		{FeatureID: "FeatureW", Name: "Amazing API"},
+	}
+	for _, feature := range webFeatures {
+		err := client.UpsertWebFeature(ctx, feature)
+		if err != nil {
+			t.Errorf("unexpected error during insert of features. %s", err.Error())
+		}
+	}
+
+	browserReleases := []BrowserRelease{
+		// fooBrowser Releases
+		{BrowserName: "fooBrowser", BrowserVersion: "99", ReleaseDate: time.Date(2023, 12, 5, 0, 0, 0, 0, time.UTC)},
+		{BrowserName: "fooBrowser", BrowserVersion: "100", ReleaseDate: time.Date(2024, 1, 10, 0, 0, 0, 0, time.UTC)},
+		{BrowserName: "fooBrowser", BrowserVersion: "101", ReleaseDate: time.Date(2024, 4, 5, 0, 0, 0, 0, time.UTC)},
+
+		// barBrowser Releases
+		{BrowserName: "barBrowser", BrowserVersion: "80", ReleaseDate: time.Date(2023, 11, 15, 0, 0, 0, 0, time.UTC)},
+		{BrowserName: "barBrowser", BrowserVersion: "81", ReleaseDate: time.Date(2024, 1, 20, 0, 0, 0, 0, time.UTC)},
+	}
+	for _, release := range browserReleases {
+		err := client.InsertBrowserRelease(ctx, release)
+		if err != nil {
+			t.Errorf("unexpected error during insert of releases. %s", err.Error())
+		}
+	}
+
+	browserFeatureAvailabilities := []BrowserFeatureAvailability{
+		// fooBrowser Availabilities
+		{BrowserName: "fooBrowser", BrowserVersion: "100", FeatureID: "FeatureX"}, // Available from fooBrowser 100
+		{BrowserName: "fooBrowser", BrowserVersion: "100", FeatureID: "FeatureY"},
+		{BrowserName: "fooBrowser", BrowserVersion: "101", FeatureID: "FeatureZ"}, // Available from fooBrowser 101
+
+		// barBrowser Availabilities
+		{BrowserName: "barBrowser", BrowserVersion: "80", FeatureID: "FeatureY"}, // Available from barBrowser 80
+		{BrowserName: "barBrowser", BrowserVersion: "81", FeatureID: "FeatureW"}, // Available from barBrowser 81
+	}
+	for _, availability := range browserFeatureAvailabilities {
+		err := client.InsertBrowserFeatureAvailability(ctx, availability)
+		if err != nil {
+			t.Errorf("unexpected error during insert. %s", err.Error())
+		}
+	}
+}
+
+func TestListBrowserFeatureCountMetric(t *testing.T) {
+	client := getTestDatabase(t)
+	ctx := context.Background()
+
+	loadDataForListBrowserFeatureCountMetric(ctx, t, client)
+
+	// Test 1a. First Page
+	browser := "fooBrowser"
+	startAt := time.Date(2023, 12, 1, 0, 0, 0, 0, time.UTC)
+	endAt := time.Date(2024, 5, 1, 0, 0, 0, 0, time.UTC)
+	pageSize := 2
+
+	result, err := client.ListBrowserFeatureCountMetric(ctx, browser, startAt, endAt, pageSize, nil)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	expectedResult := &BrowserFeatureCountResultPage{
+		NextPageToken: valuePtr(encodeBrowserFeatureCountCursor(time.Date(2024, 4, 5, 0, 0, 0, 0, time.UTC), 3)),
+		Metrics: []BrowserFeatureCountMetric{
+			{
+				ReleaseDate:  time.Date(2024, 1, 10, 0, 0, 0, 0, time.UTC),
+				FeatureCount: 2,
+			},
+			{
+				ReleaseDate:  time.Date(2024, 4, 5, 0, 0, 0, 0, time.UTC),
+				FeatureCount: 3,
+			},
+		},
+	}
+
+	if !reflect.DeepEqual(expectedResult, result) {
+		t.Errorf("unexpected result.\nExpected %+v\nReceived %+v", expectedResult, result)
+	}
+
+	// Test 1b. Second Page
+	result, err = client.ListBrowserFeatureCountMetric(ctx, browser, startAt, endAt, pageSize, result.NextPageToken)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	expectedResult = &BrowserFeatureCountResultPage{
+		NextPageToken: nil,
+		Metrics:       nil,
+	}
+
+	if !reflect.DeepEqual(expectedResult, result) {
+		t.Errorf("unexpected result.\nExpected %+v\nReceived %+v", expectedResult, result)
+	}
+
+	// Test 2. Let's try to get the last one and it should get one point but still count all the features beforehand.
+	startAt = time.Date(2024, 4, 1, 0, 0, 0, 0, time.UTC)
+	endAt = time.Date(2024, 5, 1, 0, 0, 0, 0, time.UTC)
+
+	result, err = client.ListBrowserFeatureCountMetric(ctx, browser, startAt, endAt, 100, nil)
+	if err != nil {
+		t.Errorf("Unexpected error: %v", err)
+	}
+	expectedResult = &BrowserFeatureCountResultPage{
+		NextPageToken: nil,
+		Metrics: []BrowserFeatureCountMetric{
+			{
+				ReleaseDate:  time.Date(2024, 4, 5, 0, 0, 0, 0, time.UTC),
+				FeatureCount: 3,
+			},
+		},
+	}
+
+	if !reflect.DeepEqual(expectedResult, result) {
+		t.Errorf("unexpected result.\nExpected %+v\nReceived %+v", expectedResult, result)
+	}
+
+}
