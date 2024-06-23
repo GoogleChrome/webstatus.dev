26d2db223d880cbea99e45bc7afe8d3ba8e6e136
James Scott <jamescscott@google.com>
2024-06-23 15:18:26 +0000

    Fix gzip compression
    As reported in #367, the gzip compression is not working for the static
    resources.
    
    This change fixes that by enabling gzip:
    - For the static resources in the public directory
    - The index.html that is served
    
    Added a test make sure Content-Encoding is indeed coming back from the NGINX server.
    
    Change-Id: I8efe8a8d3f0cc1fed3325c0221ec2194c046a606

--- playwright.config.ts
+++ playwright.config.ts
@@ -42,6 +42,10 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
+    extraHTTPHeaders: {
+      // Must add this to simulate the headers sent by the browser.
+      'Accept-Encoding': 'gzip, deflate, br, zstd',
+    },
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
