244b7c0e9d16da76be0c71667c39636dc7937af5
James C Scott III <jcscottiii@users.noreply.github.com>
2024-08-17 12:22:31 -0400

    Add support for WebDX Groups, Feature-Group Mappings &  Group Descendants (#594)
    **Description:**
    
    This PR introduces the foundational schema and data access layer for managing WebDX Groups and their associations with Web Features in Spanner.
    
    **Key Changes**
    
    * **Schema:**
       * Created the `WebDXGroups` table to store group metadata.
       * Created the `WebFeatureGroups` table to map web features to their associated groups.
       * Created the `WebDXGroupDescendants` to include the descendant group IDs for efficient querying
    
    * **Data Access Layer:**
       * Implemented `UpsertGroup` and `GetGroupIDFromGroupKey` methods in the `Client` struct to interact with the `WebDXGroups` table.
       * Implemented `UpsertWebFeatureGroup` to manage feature-group mappings in the `WebFeatureGroups` table.
       * Implemeneted `UpsertGroupDescendantInfo` to manage mappings of groups to all descendant groups.
       * Introduced helper structs and mappers (`WebFeatureGroup`, `groupSpannerMapper`, etc.) to facilitate data mapping and persistence.
    
    **Purpose**
    
    * Enables storing and retrieving information about WebDX Groups and their relationships with Web Features.
    * Lays the groundwork for future features that rely on group-based organization and filtering.
    * Optimizes queries for features associated with a group and its descendants by pre-calculating and storing descendant group IDs.
      * This is useful because if a user queries for a group, it should return the features belonging to children groups too.

---
+++ lib/gcpspanner/web_feature_groups_test.go
@@ -0,0 +1,172 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gcpspanner
+
+import (
+	"cmp"
+	"context"
+	"errors"
+	"slices"
+	"testing"
+
+	"cloud.google.com/go/spanner"
+	"google.golang.org/api/iterator"
+)
+
+func setupRequiredTablesForWebFeatureGroup(
+	ctx context.Context,
+	t *testing.T,
+) map[string]string {
+	ret := map[string]string{}
+	sampleFeatures := getSampleFeatures()
+	for _, feature := range sampleFeatures {
+		id, err := spannerClient.UpsertWebFeature(ctx, feature)
+		if err != nil {
+			t.Errorf("unexpected error during insert. %s", err.Error())
+
+			continue
+		}
+		ret[feature.FeatureKey] = *id
+	}
+
+	return ret
+}
+
+func (c *Client) createSampleWebFeatureGroups(
+	ctx context.Context, t *testing.T, idMap map[string]string) {
+	err := c.UpsertWebFeatureGroup(ctx, WebFeatureGroup{
+		WebFeatureID: idMap["feature1"],
+		GroupIDs: []string{
+			"group1",
+			"group2",
+		},
+	})
+	if err != nil {
+		t.Fatalf("failed to insert group. err: %s group\n", err)
+	}
+	err = c.UpsertWebFeatureGroup(ctx, WebFeatureGroup{
+		WebFeatureID: idMap["feature2"],
+		GroupIDs:     nil,
+	})
+	if err != nil {
+		t.Fatalf("failed to insert group. err: %s group\n", err)
+	}
+}
+
+func (c *Client) ReadAllWebFeatureGroups(ctx context.Context, _ *testing.T) ([]WebFeatureGroup, error) {
+	stmt := spanner.NewStatement(
+		`SELECT
+			WebFeatureID, GroupIDs
+		FROM WebFeatureGroups`)
+	iter := c.Single().Query(ctx, stmt)
+	defer iter.Stop()
+
+	var ret []WebFeatureGroup
+	for {
+		row, err := iter.Next()
+		if errors.Is(err, iterator.Done) {
+			break // End of results
+		}
+		if err != nil {
+			return nil, errors.Join(ErrInternalQueryFailure, err)
+		}
+		var group spannerWebFeatureGroup
+		if err := row.ToStruct(&group); err != nil {
+			return nil, errors.Join(ErrInternalQueryFailure, err)
+		}
+		ret = append(ret, group.WebFeatureGroup)
+	}
+
+	return ret, nil
+}
+
+func sortWebFeatureGroups(left, right WebFeatureGroup) int {
+	return cmp.Compare(left.WebFeatureID, right.WebFeatureID)
+}
+
+func webFeatureGroupEquality(left, right WebFeatureGroup) bool {
+	return left.WebFeatureID == right.WebFeatureID &&
+		slices.Equal(left.GroupIDs, right.GroupIDs)
+}
+
+func TestUpsertWebFeatureGroup(t *testing.T) {
+	restartDatabaseContainer(t)
+	ctx := context.Background()
+	idMap := setupRequiredTablesForWebFeatureGroup(ctx, t)
+	spannerClient.createSampleWebFeatureGroups(ctx, t, idMap)
+
+	expected := []WebFeatureGroup{
+		{
+			WebFeatureID: idMap["feature1"],
+			GroupIDs: []string{
+				"group1",
+				"group2",
+			},
+		},
+		{
+			WebFeatureID: idMap["feature2"],
+			GroupIDs:     nil,
+		},
+	}
+	slices.SortFunc(expected, sortWebFeatureGroups)
+
+	groups, err := spannerClient.ReadAllWebFeatureGroups(ctx, t)
+	if err != nil {
+		t.Fatalf("unable to get all groups err: %s", err)
+	}
+	slices.SortFunc(groups, sortWebFeatureGroups)
+
+	if !slices.EqualFunc(expected, groups, webFeatureGroupEquality) {
+		t.Errorf("unequal groups.\nexpected %+v\nreceived %+v", expected, groups)
+	}
+
+	// Upsert group
+	err = spannerClient.UpsertWebFeatureGroup(ctx, WebFeatureGroup{
+		WebFeatureID: idMap["feature2"],
+		GroupIDs: []string{
+			"group3",
+		},
+	})
+	if err != nil {
+		t.Fatalf("unable to update group err: %s", err)
+	}
+
+	expected = []WebFeatureGroup{
+		{
+			WebFeatureID: idMap["feature1"],
+			GroupIDs: []string{
+				"group1",
+				"group2",
+			},
+		},
+		{
+			WebFeatureID: idMap["feature2"],
+			GroupIDs: []string{
+				"group3",
+			},
+		},
+	}
+	slices.SortFunc(expected, sortWebFeatureGroups)
+
+	groups, err = spannerClient.ReadAllWebFeatureGroups(ctx, t)
+	if err != nil {
+		t.Fatalf("unable to get all groups err: %s", err)
+	}
+	slices.SortFunc(groups, sortWebFeatureGroups)
+
+	if !slices.EqualFunc(expected, groups, webFeatureGroupEquality) {
+		t.Errorf("unequal groups.\nexpected %+v\nreceived %+v", expected, groups)
+	}
+}
