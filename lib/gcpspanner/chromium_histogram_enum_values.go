3a8bd247a42d2b73fdc9a3b7a195f183197053ee
James C Scott III <jcscottiii@users.noreply.github.com>
2024-09-18 11:26:28 -0400

    Add Spanner functionality to store and retrieve Chromium Histogram Enums and EnumValues (#672)
    This change builds on the landed schema changes in #665.
    
    Background:
    - This change adds functionality to store and retrieve Chromium Histogram Enums and their values in Spanner.
    This data will be used in future changes to associate enum values with feature keys and store daily metrics.
    
    This is the first part of the background where we are only storing the enum and enum values
    from the enums.xml file.
    
    Table: ChromiumHistogramEnums
    - It implements the writeableEntityMapperWithIDRetrieval [interface](https://github.com/GoogleChrome/webstatus.dev/blob/976ac09c7431204205970a566ef94db42b8e4456/lib/gcpspanner/client.go#L202-L205)
    - Expose two methods:
      - UpsertChromiumHistogramEnum - for upserting values
      - GetIDFromChromiumHistogramKey - for getting the internal ID since we will be using the ID as the foreign key ChromiumHistogramEnumID in the tables ChromiumHistogramEnumValues and DailyChromiumHistogramEnumCapstones.
    
    Table: ChromiumHistogramEnumValues
    - It implements the writeableEntityMapperWithIDRetrieval [interface](https://github.com/GoogleChrome/webstatus.dev/blob/976ac09c7431204205970a566ef94db42b8e4456/lib/gcpspanner/client.go#L202-L205)
    - Expose two methods:
      - UpsertChromiumHistogramEnumValue - for upserting values
      - GetIDFromChromiumHistogramEnumValueKey - for getting the internal ID since we will be using the ID as the foreign key ChromiumHistogramEnumValueID in the tables WebFeatureChromiumHistogramEnumValues and DailyChromiumHistogramMetrics.
    
    Part of splitting up #616

---
+++ lib/gcpspanner/chromium_histogram_enum_values.go
@@ -0,0 +1,106 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gcpspanner
+
+import (
+	"context"
+	"fmt"
+
+	"cloud.google.com/go/spanner"
+)
+
+const (
+	chromiumHistogramEnumValuesTable = "ChromiumHistogramEnumValues"
+)
+
+type chromiumHistogramEnumValuesMapper struct{}
+
+func (m chromiumHistogramEnumValuesMapper) Table() string {
+	return chromiumHistogramEnumValuesTable
+}
+
+func (m chromiumHistogramEnumValuesMapper) SelectOne(key spannerChromiumHistogramEnumValueKey) spanner.Statement {
+	stmt := spanner.NewStatement(fmt.Sprintf(`
+	SELECT
+		ID, ChromiumHistogramEnumID, BucketID, Label
+	FROM %s
+	WHERE ChromiumHistogramEnumID = @chromiumHistogramEnumID AND BucketID = @bucketID
+	LIMIT 1`, m.Table()))
+	parameters := map[string]interface{}{
+		"chromiumHistogramEnumID": key.ChromiumHistogramEnumID,
+		"bucketID":                key.BucketID,
+	}
+	stmt.Params = parameters
+
+	return stmt
+}
+
+func (m chromiumHistogramEnumValuesMapper) GetKey(in ChromiumHistogramEnumValue) spannerChromiumHistogramEnumValueKey {
+	return spannerChromiumHistogramEnumValueKey{
+		ChromiumHistogramEnumID: in.ChromiumHistogramEnumID,
+		BucketID:                in.BucketID,
+	}
+}
+
+func (m chromiumHistogramEnumValuesMapper) Merge(
+	_ ChromiumHistogramEnumValue, existing spannerChromiumHistogramEnumValue) spannerChromiumHistogramEnumValue {
+	// If the histogram exists, it currently does nothing and keeps the existing as-is.
+	return existing
+}
+
+func (m chromiumHistogramEnumValuesMapper) GetID(in spannerChromiumHistogramEnumValueKey) spanner.Statement {
+	stmt := spanner.NewStatement(fmt.Sprintf(`
+	SELECT
+		ID
+	FROM %s
+	WHERE ChromiumHistogramEnumID = @chromiumHistogramEnumID AND BucketID = @bucketID
+	LIMIT 1`, m.Table()))
+	parameters := map[string]interface{}{
+		"chromiumHistogramEnumID": in.ChromiumHistogramEnumID,
+		"bucketID":                in.BucketID,
+	}
+	stmt.Params = parameters
+
+	return stmt
+}
+
+type ChromiumHistogramEnumValue struct {
+	ChromiumHistogramEnumID string `spanner:"ChromiumHistogramEnumID"`
+	BucketID                int64  `spanner:"BucketID"`
+	Label                   string `spanner:"Label"`
+}
+
+type spannerChromiumHistogramEnumValue struct {
+	ID string `spanner:"ID"`
+	ChromiumHistogramEnumValue
+}
+
+type spannerChromiumHistogramEnumValueKey struct {
+	ChromiumHistogramEnumID string
+	BucketID                int64
+}
+
+func (c *Client) UpsertChromiumHistogramEnumValue(ctx context.Context, in ChromiumHistogramEnumValue) (*string, error) {
+	return newEntityWriterWithIDRetrieval[chromiumHistogramEnumValuesMapper, string](c).upsertAndGetID(ctx, in)
+}
+
+func (c *Client) GetIDFromChromiumHistogramEnumValueKey(
+	ctx context.Context, chromiumHistogramEnumID string, bucketID int64) (*string, error) {
+	return newEntityWriterWithIDRetrieval[chromiumHistogramEnumValuesMapper, string](c).
+		getIDByKey(ctx, spannerChromiumHistogramEnumValueKey{
+			ChromiumHistogramEnumID: chromiumHistogramEnumID,
+			BucketID:                bucketID,
+		})
+}
---
+++ lib/gcpspanner/chromium_histogram_enums_test.go
@@ -0,0 +1,104 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gcpspanner
+
+import (
+	"context"
+	"errors"
+	"slices"
+	"testing"
+
+	"cloud.google.com/go/spanner"
+	"google.golang.org/api/iterator"
+)
+
+func getSampleChromiumHistogramEnums() []ChromiumHistogramEnum {
+	return []ChromiumHistogramEnum{
+		{
+			HistogramName: "AnotherHistogram",
+		},
+		{
+			HistogramName: "WebDXFeatureObserver",
+		},
+	}
+}
+
+func insertSampleChromiumHistogramEnums(ctx context.Context, t *testing.T, c *Client) map[string]string {
+	enums := getSampleChromiumHistogramEnums()
+	m := make(map[string]string, len(enums))
+	for _, enum := range enums {
+		id, err := c.UpsertChromiumHistogramEnum(ctx, enum)
+		if err != nil {
+			t.Fatalf("unable to insert sample histogram enums. error %s", err)
+		}
+		m[enum.HistogramName] = *id
+	}
+
+	return m
+}
+
+// Helper method to get all the enums in a stable order.
+func (c *Client) ReadAllChromiumHistogramEnums(ctx context.Context, t *testing.T) ([]ChromiumHistogramEnum, error) {
+	stmt := spanner.NewStatement(
+		`SELECT
+			ID, HistogramName
+		FROM ChromiumHistogramEnums
+		ORDER BY HistogramName ASC`)
+	iter := c.Single().Query(ctx, stmt)
+	defer iter.Stop()
+
+	var ret []ChromiumHistogramEnum
+	for {
+		row, err := iter.Next()
+		if errors.Is(err, iterator.Done) {
+			break // End of results
+		}
+		if err != nil {
+			return nil, errors.Join(ErrInternalQueryFailure, err)
+		}
+		var enum spannerChromiumHistogramEnum
+		if err := row.ToStruct(&enum); err != nil {
+			return nil, errors.Join(ErrInternalQueryFailure, err)
+		}
+		if enum.ID == "" {
+			t.Error("retrieved enum ID is empty")
+		}
+		ret = append(ret, enum.ChromiumHistogramEnum)
+	}
+
+	return ret, nil
+}
+
+func TestUpsertChromiumHistogramEnum(t *testing.T) {
+	restartDatabaseContainer(t)
+	ctx := context.Background()
+	insertSampleChromiumHistogramEnums(ctx, t, spannerClient)
+	enums, err := spannerClient.ReadAllChromiumHistogramEnums(ctx, t)
+	if err != nil {
+		t.Errorf("unexpected error during read all. %s", err.Error())
+	}
+	sampleHistogramsEnums := getSampleChromiumHistogramEnums()
+	if !slices.Equal[[]ChromiumHistogramEnum](getSampleChromiumHistogramEnums(), enums) {
+		t.Errorf("unequal enums. expected %+v actual %+v", sampleHistogramsEnums, enums)
+	}
+
+	_, err = spannerClient.UpsertChromiumHistogramEnum(ctx, ChromiumHistogramEnum{
+		HistogramName: "WebDXFeatureObserver",
+	})
+	if err != nil {
+		t.Errorf("unexpected error during update. %s", err.Error())
+	}
+	// TODO: Try to upsert with the generated UUID.
+}
