37adcfa9de2b521b3a6c9764b0c012aa20910471
James Scott <jamescscott@google.com>
2024-09-19 21:05:03 +0000

    Refactor: Make reusable modules for creating jobs
    Upon doing #616, I saw I was once again creating a lot of terraform files to deploy the jobs.
    And a lot of the terraform was a repeat of previous jobs with slight name tweaks.
    
    This change introduces three new reusable modules:
    - go_image - useful for any of the Go images built for the project
    - job - useful for deploying an image to a cloud run job. creates the service account and adds permissions as needed
    - single_stage_go_workflow - all of the workflows are single jobs (not a chain of multiple jobs.). This adds a common workflow template for one job, creates a schedule and gives it the permission to invoke the job.
    
    After this change has landed, developers can simply make a change like the following to deploy a job (instead of creating multiple files):
    
    ```
    module "bcd_workflow" {
      source = "../modules/single_stage_go_workflow"
      providers = {
        google.internal_project = google.internal_project
        google.public_project   = google.public_project
      }
      regions               = var.regions
      short_name            = "bcd-wrkflw"
      full_name             = "BCD Workflow"
      deletion_protection   = var.deletion_protection
      project_id            = var.spanner_datails.project_id
      timeout_seconds       = 60 * 5 # 5 minutes
      image_name            = "bcd_consumer_image"
      spanner_details       = var.spanner_datails
      env_id                = var.env_id
      region_schedules      = var.bcd_region_schedules
      docker_repository_url = var.docker_repository_details.url
      go_module_path        = "workflows/steps/services/bcd_consumer"
      env_vars = [
        {
          name  = "PROJECT_ID"
          value = var.spanner_datails.project_id
        },
        {
          name  = "SPANNER_DATABASE"
          value = var.spanner_datails.database
        },
        {
          name  = "SPANNER_INSTANCE"
          value = var.spanner_datails.instance
        }
      ]
    }
    
    ```

---
+++ infra/modules/go_image/main.tf
@@ -0,0 +1,46 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+resource "docker_image" "local_image" {
+  name = "${var.docker_repository_url}/${var.image_name}"
+  build {
+    context = "${path.cwd}/.."
+    build_args = {
+      service_dir : var.go_module_path
+      MAIN_BINARY : var.binary_type
+    }
+    dockerfile = "images/go_service.Dockerfile"
+  }
+  triggers = {
+    dir_sha1 = sha1(join("",
+      # Rebuild the image if anything changes in the go module itself.
+      [for f in fileset(path.cwd, "/../${var.go_module_path}/**") : filesha1(f)],
+      # Rebuild the go image if anything changes in the lib directory.
+      [for f in fileset(path.cwd, "/../lib/**") : filesha1(f)]
+    ))
+  }
+}
+
+resource "docker_registry_image" "remote_image" {
+  name          = docker_image.local_image.name
+  keep_remotely = true
+  triggers = {
+    dir_sha1 = sha1(join("",
+      # Rebuild the image if anything changes in the go module itself.
+      [for f in fileset(path.cwd, "/../${var.go_module_path}/**") : filesha1(f)],
+      # Rebuild the go image if anything changes in the lib directory.
+      [for f in fileset(path.cwd, "/../lib/**") : filesha1(f)]
+    ))
+  }
+}
