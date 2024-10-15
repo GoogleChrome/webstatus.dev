925c0cf606fa2dda80a51638c4ee129dd0d77beb
James C Scott III <jcscottiii@users.noreply.github.com>
2024-10-15 14:01:17 -0400

    Upgrade @vaadin/router to resolve dependabot issue. (#759)
    There's a dependabot alert on path-to-regexp which vaadin/router depends on.
    
    https://github.com/GoogleChrome/webstatus.dev/security/dependabot/46
    
    This change bumps the version of vaadin/router so that it uses the fixed version.

--- frontend/src/static/js/contexts/router-context.ts
+++ frontend/src/static/js/contexts/router-context.ts
@@ -14,9 +14,11 @@
  * limitations under the License.
  */
 
-import type {Router} from '@vaadin/router';
+import type {AnyObject, Router} from '@vaadin/router';
 
 import {createContext} from '@lit/context';
 
 export type {Router} from '@vaadin/router';
-export const routerContext = createContext<Router | undefined>('router');
+export const routerContext = createContext<
+  Router<AnyObject, AnyObject> | undefined
+>('router');
