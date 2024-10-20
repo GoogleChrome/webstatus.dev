941d7160f696e38863e48b3c1276d074c208be20
kristiankrafcik <K.krafcik02@gmail.com>
2024-10-20 17:56:19 +0000

    #github-username add octokit to frontend, implement octokit call for github username

--- frontend/src/static/js/services/webstatus-firebase-auth-service.ts
+++ frontend/src/static/js/services/webstatus-firebase-auth-service.ts
@@ -33,17 +33,15 @@ import {
   signInWithPopup,
 } from 'firebase/auth';
 import {
-  User,
   FirebaseUser,
   firebaseUserContext,
 } from '../contexts/firebase-user-context.js';
 import {ServiceElement} from './service-element.js';
+import {Octokit} from '@octokit/rest';
 interface FirebaseAuthSettings {
   emulatorURL: string;
   tenantID: string;
 }
-
-const GITHUB_USERNAME_REQUEST_URL: string = 'https://api.github.com/user/';
 
 @customElement('webstatus-firebase-auth-service')
 export class WebstatusFirebaseAuthService extends ServiceElement {
@@ -68,30 +66,10 @@ export class WebstatusFirebaseAuthService extends ServiceElement {
   // Useful for testing
   emulatorConnector: (auth: Auth, url: string) => void = connectAuthEmulator;
 
-  handleGithubUsernameError(res: Response, email: string) {
-    const err = new Error();
-    if (res.status === 403 && res.headers.get('X-RateLimit-Remaining') == '0') {
-      const resetsAtMS = Number(`${res.headers.get('X-RateLimit-Reset')}000`);
-      err.message = `Status: ${res.status}, Rate limit exceeded, try again in ${Math.ceil((resetsAtMS - Date.now()) / 60000)}m`;
-    } else if (res.status === 404) {
-      err.message = `Status: ${res.status}, Could not find user data for github: ${email}`;
-    } else {
-      // add other cases if you want to handle them
-      err.message = `Unexpected status code: ${res.status}`;
-    }
-    throw err;
-  }
-
-  async getGithubUser(token: string): Promise<any> {
-    return fetch(`${GITHUB_USERNAME_REQUEST_URL}`, {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${token}`,
-        Accept: 'application/vnd.github+json',
-        'X-GitHub-Api-Version': '2022-11-28',
-      },
-    });
-  }
+  loadGithubUsername = async (token?: string) => {
+    const octokit = new Octokit({auth: token});
+    return await octokit.users.getAuthenticated();
+  };
 
   initFirebaseAuth() {
     if (this.firebaseApp) {
@@ -117,46 +95,24 @@ export class WebstatusFirebaseAuthService extends ServiceElement {
       // Set up the callback that will detect when:
       // 1. The user first logs in
       // 2. Resuming a session
-      this.firebaseAuthConfig.auth.onAuthStateChanged((user: User | null) => {
-        if (user !== null) {
-          this.user =
-            user !== null ? {...user, gitHubUsername: 'empty'} : undefined;
-          this._loadingGithubUsername = new Task(this, {
-            args: (): [any] => [this.user],
-            task: async ([firebaseUser]: [any]) => {
-              try {
-                if (
-                  firebaseUser &&
-                  typeof firebaseUser !== 'string' &&
-                  firebaseUser.accessToken
-                ) {
-                  const res: Response = await this.getGithubUser(
-                    firebaseUser.accessToken
-                  );
-                  if (res.ok) {
-                    const data = await res.json();
-                    if (this.user !== undefined) {
-                      this.user = {
-                        ...this.user,
-                        gitHubUsername: data.login,
-                      };
-                    }
-                  } else {
-                    this.handleGithubUsernameError(res, this.user?.email || '');
-                  }
-                } else {
-                  throw new Error('Github username request failed.');
-                }
-              } catch (e: any) {
-                console.error(e.message);
-              }
-            },
-          });
-          this._loadingGithubUsername.run();
-        } else {
-          throw new Error('Github username request failed.');
+      this.firebaseAuthConfig.auth.onAuthStateChanged(
+        async (authenticatedUser?: any) => {
+          if (authenticatedUser !== null) {
+            await this.loadGithubUsername(authenticatedUser.accessToken)
+              .then(githubUser => {
+                this.user = {
+                  ...authenticatedUser,
+                  gitHubUsername: githubUser.data.login || null,
+                };
+              })
+              .catch(_ => {
+                throw new Error('Github username request failed.');
+              });
+          } else {
+            throw new Error('No user authenticated.');
+          }
         }
-      });
+      );
     }
   }
 
