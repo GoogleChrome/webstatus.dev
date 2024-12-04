28f22dd3ce0ba39abeabe8e94b515087f3f9077f
kyleju <kyleju@google.com>
2024-12-04 00:41:41 +0000

    State of css 2024

--- frontend/src/static/js/utils/constants.ts
+++ frontend/src/static/js/utils/constants.ts
@@ -36,4 +36,11 @@ export const DEFAULT_BOOKMARKS: Bookmark[] = [
     query: 'baseline_date:2023-01-01..2023-12-31',
     description: 'All Baseline 2023 features',
   },
+  {
+    name: 'Top CSS Interop issues',
+    query:
+      'id:anchor-positioning OR id:container-queries OR id:has OR id:nesting OR id:view-transitions OR id:subgrid OR id:grid OR name:scrollbar OR id:scroll-driven-animations OR id:scope',
+    description:
+      "This list reflects the top 10 interoperability pain points identified by developers in the State of CSS 2024 survey, https://2024.stateofcss.com/en-US/usage/#css_interoperability_features. We have also included their implementation status across Baseline browsers. You will notice that in some cases the items are already Baseline features, but may not have have been Baseline for long enough for developers to use with their target audience's browser support requirements. Since some voted-on pain points involve multiple web features, the list extends beyond 10 individual items for clarity and comprehensive coverage.",
+  },
 ];
