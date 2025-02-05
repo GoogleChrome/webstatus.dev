e1ea4cdaa4242cb738e9ca3cc9360b2f2e55de29
James Scott <jamescscott@google.com>
2025-02-05 00:14:08 +0000

    use correct waiting

--- e2e/tests/feature-page.spec.ts
+++ e2e/tests/feature-page.spec.ts
@@ -92,7 +92,7 @@ test('chart width resizes with window', async ({page}) => {
 test('date range changes are preserved in the URL', async ({page}) => {
   await page.goto('http://localhost:5555/features/odit64');
   await page.waitForSelector('#feature-support-chart-container');
-  await page.waitForTimeout(1000);
+  await page.waitForSelector('.spinner-container', {state: 'detached'});
 
   // Get the current default startDate and endDate from the selectors
   // TODO Figure out how to use getByLabel with shoelace and replace page.locator with that.
@@ -114,8 +114,6 @@ test('date range changes are preserved in the URL', async ({page}) => {
 
   // Submit the change
   await submitBtn.click();
-  // TODO: Remove the timeout. Currently webkit is flaky.
-  await page.waitForTimeout(3000);
 
   // Check that the URL includes the startDate and endDate
   const url = page.url();
