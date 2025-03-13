8d7d31e77a3fb9d5d6f9f084b98e0aa6c37eb53f
James C Scott III <jcscottiii@users.noreply.github.com>
2025-03-13 13:41:34 +0000

    Use K_REVISION for SPA bundle (#1270)
    This should allow browsers to request a new bundle after a deployment
    
    Cloud run environment provides [K_REVISION](https://cloud.google.com/run/docs/container-contract#:~:text=hello%2Dworld-,K_REVISION,-The%20name%20of)
    
    So we can use that as an unique ID. We base 64 url encode it so that it is safe for browsers.
    
    Also, set the local environment up with a fake value so that it replicates production.

--- frontend/scripts/setup_server.sh
+++ frontend/scripts/setup_server.sh
@@ -16,6 +16,8 @@
 INDEX_HTML="/usr/share/nginx/html/index.html"
 TMP_INDEX_HTML="/tmp/index.html"
 OLD_INDEX_HTML="/tmp/old-index.html"
+# shellcheck disable=SC2155 # Used by index.html
+export WEBSTATUS_VERSION="$(echo "$K_REVISION" | basenc --base64url)"
 envsubst < "${INDEX_HTML}" > "${TMP_INDEX_HTML}"
 cp "${INDEX_HTML}" "${OLD_INDEX_HTML}"
 cp "${TMP_INDEX_HTML}" "${INDEX_HTML}"
