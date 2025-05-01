1bdc82ee95d35788b8e42223dfa5abeba06757e5
James C Scott III <jcscottiii@users.noreply.github.com>
2025-05-01 17:56:40 +0000

    Improve unit test ability for frontend code (#1453)
    Previously, while running web test runner, it could not find the source code.
    
    As a result, a developer could not insert break points.
    
    This fixes that.

--- frontend/web-test-runner.config.mjs
+++ frontend/web-test-runner.config.mjs
@@ -34,12 +34,12 @@ export default /** @type {import("@web/test-runner").TestRunnerConfig} */ ({
   },
 
   // in a monorepo you need to set the root dir to resolve modules
-  rootDir: 'build/',
+  rootDir: '.',
 
   files: [
     // Have to compile tests
     // Taken from https://github.com/open-wc/create/blob/master/src/generators/testing-wtr-ts/templates/static/web-test-runner.config.mjs
-    '**/test/*.test.js',
+    'build/**/test/*.test.js',
   ],
   testRunnerHtml: testFramework => `
   <html>
