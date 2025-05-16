6965d9bb8609d66aa5bc52b356e9bf5e130e51cd
James C Scott III <jcscottiii@users.noreply.github.com>
2025-05-16 16:08:02 +0000

    Fix playwright test flakiness (#1495)
    * Fix playwright test flakiness
    
    During the saved search playwright tests, we kill and reopen the ports needed for the database (as well as all other ports).
    
    We do this because beforehand, we did not port forward the ports for the database and only the ports needed to connect to the app (the backend already had access to the database internally)
    
    But instead of doing that, we now port forward the database ports. Then we have a new helper makefile target to check if the port is live.
    
    We use `nc` instead of curl because it did not work with grpc. The devcontainer Dockerfile has been updated to install that dependency (devs will need to rebuild their devcontainer)
    
    Hopefully, this will reduce the times when the port fails to forward again on subsequent tries since we never have to forward it again.
    
    One thing to note: Now developers will need to run make port-forward-manual first before dev_fake_data and other commands after running start-local
    
    * revert limit for datastore
    
    * further increase spanner limit
    
    * dev_fake_data already calls check-local-ports

--- e2e/tests/utils.ts
+++ e2e/tests/utils.ts
@@ -122,19 +122,12 @@ export async function resetUserData() {
   const projectRootDir = path.resolve(__dirname, '../..');
 
   try {
-    const cmd1 = `make dev_fake_data -o build -o is_local_migration_ready LOAD_FAKE_DATA_FLAGS='-scope=user -reset'`;
+    const cmd = `make dev_fake_data -o build -o is_local_migration_ready LOAD_FAKE_DATA_FLAGS='-scope=user -reset'`;
 
-    console.log(`Executing command: ${cmd1} in ${projectRootDir}`);
-    execSync(cmd1, {cwd: projectRootDir, stdio: 'inherit'});
+    console.log(`Executing command: ${cmd} in ${projectRootDir}`);
+    execSync(cmd, {cwd: projectRootDir, stdio: 'inherit'});
 
     console.log('Reset command finished successfully.');
-
-    const cmd2 = 'make port-forward-manual';
-    console.log(`Executing command: ${cmd2} in ${projectRootDir}`);
-    execSync(cmd2, {
-      cwd: projectRootDir,
-      stdio: 'inherit',
-    });
   } catch (error) {
     console.error('Error reset command (make dev_fake_data):', error);
     throw new Error('Reset command finished, halting tests.');
