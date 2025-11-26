08dbbc8bd316e32ca43283fe653195c03cd59fe3
James Scott <jamescscott@google.com>
2025-11-26 23:19:18 +0000

    feat(httpserver): add list subscriptions endpoint
    This change introduces a new HTTP endpoint to list user subscriptions. It includes
    the necessary request handling, response formatting, and unit tests to ensure
    correct functionality. The endpoint supports pagination.

--- backend/pkg/httpserver/server_test.go
+++ backend/pkg/httpserver/server_test.go
@@ -1235,6 +1235,16 @@ func (m *mockServerInterface) ListMissingOneImplementationFeatures(ctx context.C
 	panic("unimplemented")
 }
 
+// ListSubscriptions implements backend.StrictServerInterface.
+// nolint: ireturn // WONTFIX - generated method signature
+func (m *mockServerInterface) ListSubscriptions(ctx context.Context,
+	_ backend.ListSubscriptionsRequestObject) (
+	backend.ListSubscriptionsResponseObject, error) {
+	assertUserInCtx(ctx, m.t, m.expectedUserInCtx)
+	m.callCount++
+	panic("unimplemented")
+}
+
 func (m *mockServerInterface) assertCallCount(expectedCallCount int) {
 	if m.callCount != expectedCallCount {
 		m.t.Errorf("expected mock server to be used %d times. only used %d times", expectedCallCount, m.callCount)
