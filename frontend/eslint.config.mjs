aa7b75cb93e5f4a72c6d81d44a625e417e67bd8a
James Scott <jamescscott@google.com>
2026-01-14 00:15:55 +0000

    update eslint file

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -15,24 +15,13 @@
  */
 
 import {defineConfig} from 'eslint/config';
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tseslint from 'typescript-eslint';
 import globals from 'globals';
-import tsParser from '@typescript-eslint/parser';
-import path from 'node:path';
-import {fileURLToPath} from 'node:url';
-import js from '@eslint/js';
-import {FlatCompat} from '@eslint/eslintrc';
+import {configs as liteslint} from 'eslint-plugin-lit';
 import gts from 'gts';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
-
 export default defineConfig([
+  tseslint.configs.recommended,
   ...gts,
   {
     ignores: [
@@ -44,20 +33,14 @@ export default defineConfig([
       'coverage/lcov-report/*',
     ],
   },
-  ...compat.extends('plugin:lit/recommended'),
   {
-    plugins: {
-      '@typescript-eslint': typescriptEslint,
-    },
-
+    extends: [liteslint['flat/recommended']],
+  },
+  {
     languageOptions: {
       globals: {
         ...globals.browser,
       },
-
-      parser: tsParser,
-      ecmaVersion: 'latest',
-      sourceType: 'module',
     },
 
     rules: {
